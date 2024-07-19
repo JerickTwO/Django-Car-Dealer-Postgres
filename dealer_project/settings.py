5240f6a6d05e1c01ee8fe300456dfd7b7ac1a6c4
JerickTwO <jerickjjtm774@gmail.com>
2024-07-19 17:23:21 -0500

    whitenoise

--- dealer_project/settings.py
+++ dealer_project/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserve_nostatic',
+    'whitenoise.runserver_nostatic',
     'car_dealer',
 ]
 
