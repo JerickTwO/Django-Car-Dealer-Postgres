00a1fc97b1bc0d1c64a7e0a63fe5d3abbe54e8cd
JerickTwO <jerickjjtm774@gmail.com>
2024-07-19 19:01:46 -0500

    settings and Procfile

--- dealer_project/settings.py
+++ dealer_project/settings.py
@@ -113,7 +113,7 @@ USE_I18N = True
 USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
