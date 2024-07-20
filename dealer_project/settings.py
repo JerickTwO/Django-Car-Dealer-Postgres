0969c915dbff029ce8e8b2e1050c145503b7e045
JerickTwO <jerickjjtm774@gmail.com>
2024-07-19 19:12:40 -0500

    database

--- dealer_project/settings.py
+++ dealer_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,7 @@ WSGI_APPLICATION = 'dealer_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgresql://postgres:lKUJBdKZIZFWETNLxGHhBqBiyKBfVuiT@postgres.railway.internal:5432/railway')
 }
 
 
